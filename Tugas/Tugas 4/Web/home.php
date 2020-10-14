0e8c6d140a0766f741724eeb9ec9597f733fa43e
DwiWahyuEffendi <dwiwahyueffendi22@gmail.com>
2020-10-15 01:25:49 +0700

    update

---
+++ Tugas/Tugas 4/Web/home.php
@@ -0,0 +1,42 @@
+<!DOCTYPE html>
+<html>
+    <head>
+        <title>Web CV</title>
+        <meta charset="UTF-8">
+        <meta name="author" content="Dwi Wahyu Effendi">
+        <link rel="shortcut icon" href="Image/Home/myphoto.jpg">
+        <link rel="stylesheet" href="style.css">
+    </head>
+    <body>
+        <img class="foto" src="Image/Home/myphoto.jpg">
+        <div class="Home">
+            <table>
+            <h1>Dwi Wahyu Effendi</h1>
+            <tr>
+                <td><img src="Image/Home/calendar.png"></td>
+                <td><h4>31 May 2000</h4></td>
+            </tr>
+            <tr>
+                <td><img src="Image/Home/location.png"></td>
+                <td><h4>Surabaya</h4></td>
+            </tr>
+            <tr>
+                <td><img src="Image/Home/phone.png"></td>
+                <td><h4>081515427617</h4></td>
+            </tr>
+            <tr>
+                <td><img src="Image/Home/email.png"></td>
+                <td><h4>dwe22@gmail.com</h4></td>
+            </tr>
+            <tr>
+                <td><img src="Image/Home/github.png"></td>
+                <td><h4>dwiwahyueffendi</h4></td>
+            </tr>
+            <tr>
+                <td><img src="Image/Home/linkedin.png"></td>
+                <td><h4>dwiwahyueffendi</h4></td>
+            </tr>
+            </table>
+        </div>
+    </body>
+</html>
