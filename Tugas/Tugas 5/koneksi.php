3b3eb3cf67c7b979c5bfb9708ea76c04ec4a129d
DwiWahyuEffendi <dwiwahyueffendi22@gmail.com>
2020-10-28 15:16:47 +0700

    Tugas 5

---
+++ Tugas/Tugas 5/koneksi.php
@@ -0,0 +1,29 @@
+<?php 
+	function connection() {
+	   $dbServer = 'localhost';
+	   $dbUser = 'root';
+	   $dbPass = '';
+	   $dbName = "mydata";
+
+	   $conn = mysqli_connect($dbServer, $dbUser, $dbPass, $dbName);
+
+	   if(!$conn)
+		  die('Koneksi anda gagal: ' . mysqli_error());
+
+	   mysqli_select_db($conn,$dbName);
+	   return $conn;
+	}
+
+	$conn = connection();
+
+	function query($query){
+		global $conn;
+		$result = mysqli_query($conn, $query); 
+		$record = [];
+
+		while( $data = mysqli_fetch_assoc($result))
+			$record[] = $data;
+
+		return $record;
+	}
+?>
